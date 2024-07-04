f802bef16a737497780b50871c4f3e59a75c97c1
zorgoz <zoltan.zorgo@gmail.com>
2024-07-04 14:57:26 +0200

    added environment awareness

--- rmq-tsl-test/Program.cs
+++ rmq-tsl-test/Program.cs
@@ -1,14 +1,15 @@
 ﻿using RabbitMQ.Client;
 using System.Net.Security;
-using System.Net;
 using System.Net.Sockets;
-using System.Security.Cryptography.X509Certificates;
+using System.Runtime.InteropServices;
 
-string server = "rabbitmq";
+string server = RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? "rabbitmq" : "localhost";
 int port = 5671;
 string pfxFilePath = "../certs/generic_user_certificate.pfx";
 
 Console.WriteLine($"Using cert file: {new FileInfo(pfxFilePath).FullName}");
+Console.WriteLine($"Trying: {server}:{port}");
+Console.WriteLine($"TCP port listening: {IsPortListening(server, port)}");
 
 // try RabbitMQ connection
 var factory = new ConnectionFactory 
@@ -29,3 +30,26 @@ var factory = new ConnectionFactory
 using var connection = factory.CreateConnection();
 
 Console.WriteLine($"Connection established: {connection.Endpoint}");
+
+static bool IsPortListening(string ipAddress, int port, int timeout = 1000)
+{
+	try
+	{
+		using (var client = new TcpClient())
+		{
+			var result = client.BeginConnect(ipAddress, port, null, null);
+			var success = result.AsyncWaitHandle.WaitOne(TimeSpan.FromMilliseconds(timeout));
+			if (!success)
+			{
+				return false;
+			}
+
+			client.EndConnect(result);
+			return true;
+		}
+	}
+	catch (SocketException)
+	{
+		return false;
+	}
+}
